6531a8f6213d013cb0c503cc29d84f596226a499
Nichtea <107244767+Nichtea@users.noreply.github.com>
2024-10-30 14:40:05 +0800

    fix4

--- flask/app.py
+++ flask/app.py
@@ -34,7 +34,7 @@ db.init_app(app)  # Initialize the db with the Flask app
 #     expose_headers=["Content-Range", "X-Content-Range"])
 CORS(app, supports_credentials=True, 
      resources={r"/*": {"origins": ["https://spm-g8t7-vue.onrender.com"]}}, 
-     allow_headers=["Content-Type", "Authorization", "X-staff-ID", "X-Role", "X-Month", "X-Dept"],
+     allow_headers=["Content-Type", "Authorization", "X-userid", "X-Role", "X-Month", "X-Dept"],
      expose_headers=["Content-Range", "X-Content-Range"])
 
 # Initialize Flask-Login
