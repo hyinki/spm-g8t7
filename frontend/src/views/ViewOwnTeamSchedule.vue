75aceffd94c4336d9b329aa918ad2fe0ee4a5091
hyinki <113676721+hyinki@users.noreply.github.com>
2024-11-03 17:36:08 +0800

    update ui

--- frontend/src/views/ViewOwnTeamSchedule.vue
+++ frontend/src/views/ViewOwnTeamSchedule.vue
@@ -58,46 +58,9 @@ import HeaderStaff from '../components/HeaderStaff.vue';
 
     <!-- List View -->
     <div v-else>
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Time</th>
-            <th>Activity</th>
-            <th>Office/WFH</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="event in dummyEvents" :key="event.name">
-            <td>{{ formatDate(event.date) }}</td>
-            <td>1pm - 2pm</td> <!-- You can customize the time format -->
-            <td>Meeting</td>
-            <td>
-              <div v-if="event.type === 'office'">
-                {{ event.name }} - Office
-              </div>
-              <div v-else>
-                {{ event.name }} - WFH
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Timeblock</th>
-            <th>Staff Name</th>
-          </tr>
-          <tr v-for="requests in ownteamschedule" :key="requests.request_ID">
-            <td>{{requests.Date}}</td>
-            <td>{{requests.Timeblock}}</td>
-            <td>{{requests.staff_name}}</td>
-          </tr>
-        </thead>
-      </table>
-      <br><h4>In office list</h4>
+      
+      
+      <h3>In office list</h3>
       <div v-for="(dictionary, date_time) in ownteaminoffice">
       <h5>{{ date_time }}</h5>
       <table class="table table-striped">
@@ -115,6 +78,7 @@ import HeaderStaff from '../components/HeaderStaff.vue';
       </tr>
     </tbody>
       </table>
+      <br>
       </div>
     </div>
   </div>
